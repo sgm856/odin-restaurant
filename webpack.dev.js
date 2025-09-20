d622bb5a2beae7cb7ae9c72be381215c70310cc6
sgm856 <179048454+sgm856@users.noreply.github.com>
2025-09-20 07:44:04 -0600

    testing webpack-merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,35 +1,12 @@
 // webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-	mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-	devtool: "eval-source-map",
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-		{
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-	    },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-    ],
-};
+});
 
